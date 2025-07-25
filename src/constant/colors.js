388a88ae3d97e74946c3aaf4e51332067edac5f4
Hinaoui <marwenhinaoui@gmail.com>
2025-07-25 16:54:04 +0100

    fixed after last commit

--- src/constant/colors.js
+++ src/constant/colors.js
@@ -2,7 +2,7 @@ export const COLORS = {
   LearRed: "#EE3124",
   LearRedDark: "#8B0000",
   WHITE: "#FFFFFF",
-  bgWHITE: "#f7f7f7ff",
+  bgWHITE: "#fafafaff",
   BLACK: "#000000",
   Gray1: "#F0EFEE",
   Gray2: "#E1DFDE",
